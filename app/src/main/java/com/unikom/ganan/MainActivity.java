ed28067dc61bb3cf72f015080452fc3cfdf88d4f
catx <ivanshadow19@gmail.com>
2021-08-04 18:48:19 +0700

    add drawer

--- app/src/main/java/com/unikom/ganan/MainActivity.java
+++ app/src/main/java/com/unikom/ganan/MainActivity.java
@@ -1,16 +1,17 @@
 package com.unikom.ganan;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -23,11 +24,18 @@ public class MainActivity extends AppCompatActivity {
     EditText searchtext;
     protected ImageLoader imageLoader;
     ArrayList<GameSales> arrayList = new ArrayList<>();
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.nav_view);
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.navigation_draw_open, R.string.navigation_draw_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         imageLoader = ImageLoader.getInstance();
         imageLoader.init(ImageLoaderConfiguration.createDefault(this));
